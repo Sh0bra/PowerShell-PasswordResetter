f368ce3d6fee63a7aa68e9e53b9e322b6627a739
shinhyunsuk <hsshin@ucdavis.edu>
2019-04-23 10:19:31 -0700

    Updated GUI and added Password Check

--- Scripts/PasswordResetter.ps1
+++ Scripts/PasswordResetter.ps1
@@ -1,6 +1,6 @@
 #Required to load the XAML form and create the PowerShell Variables
 
-.\loadDialog.ps1 -XamlPath '..\Forms\PasswordResetterGUI.xaml'
+.\loadDialog.ps1 -XamlPath '..\Forms\PasswordResetterGUIv2.xaml'
 
 #######################################################################
 
@@ -15,34 +15,44 @@ function Populate-ComboBox {
     $ListOfAccounts = Get-AccountNames
 
     ForEach ($Account in $ListOfAccounts) {
-        $cbxSelectUser.Items.Add($Account) | out-null
+        $cbxLocalAccount.Items.Add($Account) | out-null
     }   
-    $cbxSelectUser.SelectedIndex = 0; 
+    $cbxLocalAccount.SelectedIndex = 0; 
 }
 
 
 function Initialize-PasswordBoxes {
-    $pwbOldPassword.PasswordChar = '*'  
-    $pwbNewPassword.PasswordChar = '*' 
-    $pwbConfirmNewPassword.PasswordChar = '*'
+    $pwbLocalNewPassword.PasswordChar = '*' 
+    $pwbLocalReEnterNewPassword.PasswordChar = '*'
 }
 
 
 #DO STUFF
 
 Populate-ComboBox
-#Initialize-PasswordBoxes
+Initialize-PasswordBoxes
 
 
 #EVENT Handler 
     
-$btnChangePassword.add_Click({  
-    $Target = $cbxSelectUser.SelectedItem.ToString()
-    $Password = $pwbNewPassword.Password.ToString()
-    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-    $TargetAccount = Get-LocalUser -Name $Target
-    $TargetAccount | Set-LocalUser -Password $SecurePassword
-    $lblOldPassword.Content = $SecurePassword
+$btnLocalChangePassword.add_Click({  
+    try {
+        $Target = $cbxLocalAccount.SelectedItem.ToString()
+        $Password = $pwbLocalNewPassword.Password.ToString()
+        if (!$Password) { throw 'Invalid Password' }
+
+        if (!($Password -eq $pwbLocalReEnterNewPassword.Password.ToString())) {
+            throw 'Password Does not Match'
+        }
+        
+        $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+        $TargetAccount = Get-LocalUser -Name $Target
+        $TargetAccount | Set-LocalUser -Password $SecurePassword
+        [System.Windows.MessageBox]::Show('Password has been changed')
+    } catch {
+        Write-Host "Error"
+        echo $_ 
+    }
 })
 
 
@@ -52,67 +62,5 @@ $btnChangePassword.add_Click({
 $xamGUI.ShowDialog() | out-null
 
 
-<#
 
-#######################################################################
-
-PS C:\> $Password = Read-Host -AsSecureString
-PS C:\> $UserAccount = Get-LocalUser -Name "User02"
-PS C:\> $UserAccount | Set-LocalUser -Password $Password
-
-function Validate-Account($Target, $ListOfAccounts){
-    echo "out loop"
-    #echo $Target
-    echo $ListOfAccounts
-    $letterArray = "a","b","c","d"
-    
-    foreach ($letter in $letterArray)
-    {
-      Write-Host $letter
-    }
-
-    ForEach ($Accounts in $ListOfAccounts) {
-        echo "In Loop"
-        echo $Account
-        echo $Target
-        #if ($Account -eq $Target) {
-        #    return $true
-        #}
-        }
-    #return $false
-    #Write-Error "Account Does Not Exist On This Computer"
-}
-
-#clear
-
-
-
-echo $ListOfAccounts
-#$Target = Read-Host -Prompt "Enter User Name"
-#$Valid = Validate-Account($Target, $ListOfAccounts)
-
-#echo $Valid
-#echo $TargetAccountName
-
-
-
-#$SecurePassword = Read-Host -AsSecureString  "Enter password" | ConvertFrom-SecureString | out-file C:\Users\shinhs\Documents\securePassword.txt
-
-#$PlainPassword = Get-Content C:\Users\shinhs\Documents\securePassword.txt
-
-
-#$NewPassword = Read-Host -AsSecureString -Prompt "Enter New Password: " | ConvertFrom-SecureString
-
-#$SecurePassword = ConvertTo-SecureString $NewPassword
-#$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
-#$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-
-#$TargetAccount = Get-LocalUser -Name $LocalAccount
-
-#[Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR) #this is an important step to keep things secure
-#echo $UnsecurePassword
-
-
-#Set-LocalUser -Name $LocalAccount -Password $SecurePassword -Confirm
-
-#>
+#stop-process -Id $PID
